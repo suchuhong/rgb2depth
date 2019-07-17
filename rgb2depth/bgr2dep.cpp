825d5a1d26f7caf655df3e2b9d46bbd1b651e520
suchuhong <1442615457@qq.com>
2019-07-17 09:53:10 +0800

    更改main

--- rgb2depth/bgr2dep.cpp
+++ rgb2depth/bgr2dep.cpp
@@ -10,7 +10,37 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	Mat img = imread("depth//depthToBgra17.jpg", -1);
+	for (int num = 1; num < 300; num++)
+	{
+		char depthToBgrabuff[256];
+		sprintf_s(depthToBgrabuff, "depth2bgra//depthToBgra%d.jpg", num);//depth2bgra路径
+		char depthbuff[256];
+		sprintf_s(depthbuff, "depth//depth%d.png", num);//depth路径
 
+		Mat depthToBgra2 = imread(depthToBgrabuff, -1);
+		Mat depth2 = imread(depthbuff, -1);
+		Mat depthToBgra;
+		depthToBgra2.copyTo(depthToBgra);
+
+
+		for (int i = 0; i < depth2.rows; i++)
+			for (int k = 0; k < depth2.cols; k++) {
+				unsigned int d = depth2.ptr<unsigned short>(i)[k];//使用 ptr不会报错
+				if (d > 1500 || d == 0)
+					for (int j = 0; j < 3; j++)
+						depthToBgra.at<Vec3b>(i, k)[j] = 0;
+			}
+
+		char bgrBuff[256];
+		sprintf_s(bgrBuff, "bgr//bgr%d.jpg", num);//保存路径
+		imwrite(bgrBuff,depthToBgra);
+		if (num % 15 == 0) {
+			imshow(bgrBuff, depthToBgra);
+			waitKey(5000);
+			destroyAllWindows();
+		}
+		
+	}
+	
 	return 0;
 }
